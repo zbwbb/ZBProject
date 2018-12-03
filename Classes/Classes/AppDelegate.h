2ee303adfc3c95e5215752c7cce9764834fd70a3
zbwbb <zhoubinxinna@sina.com>
2018-12-03 15:16:07 +0800

    submit Classes

---
+++ Classes/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Classes
+//
+//  Created by TsouMac2016 on 2018/12/3.
+//  Copyright © 2018 TsouMac2016. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
